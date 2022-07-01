a1081553f8d155afa63f172e437057b1a36b94d8
Adrian Gasiorek <adrian.gasiorek@icloud.com>
2022-07-01 14:23:59 +0200

    Add drawing path

--- src/js/components/path.js
+++ src/js/components/path.js
@@ -1,19 +1,42 @@
 class Path{
   constructor(){
     this.getElements();
+    this.renderTable();
     this.drawPath();
+  }
+  renderTable(){
+    for(let i=0;i<100;i++){
+      this.dom.wrapper.innerHTML += `<div class="square">${i}</div>`;
+    }
   }
   getElements(){
     this.dom = {};
-    this.dom.squares = document.querySelectorAll('td');
-    this.dom.wrapperSquares = document.querySelector('tbody');    
-    this.test = this.dom.squares[2];
+    this.dom.wrapper = document.querySelector('.table-wrapper');
   }
   drawPath(){
-    this.dom.wrapperSquares.addEventListener('click',function(element){
-      console.log(element.target);
-      if(element.target == 'td'){
-        element.target.classList.add('path');
+    const thisPath = this;
+    this.dom.squares = this.dom.wrapper.querySelectorAll('.square');
+    console.log(this.dom.squares);
+    this.dom.wrapper.addEventListener('click',function(event){
+      const thisTargetValue = parseInt(event.target.innerHTML);
+      console.log(thisTargetValue);
+      console.log(event.target);
+      if(
+        event.target.classList.contains('square')
+       &&
+       !thisPath.dom.wrapper.querySelector('.path')
+       ||
+         thisPath.dom.squares[thisTargetValue-1].classList.contains('path')
+       ||
+         thisPath.dom.squares[thisTargetValue+1].classList.contains('path')
+       ||
+         thisPath.dom.squares[thisTargetValue-10].classList.contains('path')
+       ||
+         thisPath.dom.squares[thisTargetValue+10].classList.contains('path')
+      ){
+        event.target.classList.add('path');
+      }else{
+        console.log('false');
       }
     });
   }
