467171e4f5011558994c16d6db0c6b0482e578f7
Adrian Gasiorek <adrian.gasiorek@icloud.com>
2022-07-04 15:10:20 +0200

    Fix drawing path

--- src/js/components/path.js
+++ src/js/components/path.js
@@ -1,38 +1,61 @@
 class Path{
   constructor(){
     this.getElements();
+
     this.renderTable();
+
+    this.initAction();
+
     this.drawPath();
   }
   renderTable(){
-    for(let i=0;i<100;i++){
-      this.dom.wrapper.innerHTML += `<div class="square">${i}</div>`;
+    let html = '';
+    for(let row=0;row<10;row++){
+      for(let col=0; col<10;col++){
+        html += `<div class="square" data-row="${row}" data-col="${col}">
+        row: ${row}
+        col: ${col}
+        </div>`;
+      }
     }
+    this.dom.wrapper.innerHTML = html;
   }
+  initAction(){
+    const thisPath = this;
+    this.dom.btn.addEventListener('click',function(event){
+      event.preventDefault();
+      
+      thisPath.dom.btn.innerHTML = 'mk';
+    });
+  }
+  
   getElements(){
     this.dom = {};
     this.dom.wrapper = document.querySelector('.table-wrapper');
+    this.dom.btn = document.querySelector('.btn-finder');
   }
   drawPath(){
     const thisPath = this;
     this.dom.squares = this.dom.wrapper.querySelectorAll('.square');
     console.log(this.dom.squares);
     this.dom.wrapper.addEventListener('click',function(event){
-      const thisTargetValue = parseInt(event.target.innerHTML);
-      console.log(thisTargetValue);
       console.log(event.target);
+      const clickedCol = parseInt(event.target.getAttribute('data-col'));
+      const clickedRow = parseInt(event.target.getAttribute('data-row'));
+      console.log('Clicked Col',clickedCol);
+      console.log('Clicked Row',clickedRow);
       if(
         event.target.classList.contains('square')
        &&
        !thisPath.dom.wrapper.querySelector('.path')
        ||
-         thisPath.dom.squares[thisTargetValue-1].classList.contains('path')
+        document.querySelector(`[data-col="${clickedCol-1}"][data-row="${clickedRow}"]`).classList.contains('path')
        ||
-         thisPath.dom.squares[thisTargetValue+1].classList.contains('path')
+        document.querySelector(`[data-col="${clickedCol+1}"][data-row="${clickedRow}"]`).classList.contains('path')
        ||
-         thisPath.dom.squares[thisTargetValue-10].classList.contains('path')
+        document.querySelector(`[data-col="${clickedCol}"][data-row="${clickedRow-1}"]`).classList.contains('path')
        ||
-         thisPath.dom.squares[thisTargetValue+10].classList.contains('path')
+        document.querySelector(`[data-col="${clickedCol}"][data-row="${clickedRow+1}"]`).classList.contains('path')
       ){
         event.target.classList.add('path');
       }else{
---
+++ src/js/settings.js
@@ -0,0 +1,28 @@
+export const select = {
+  templateOf: {
+    finder: '#template-finder',
+  },
+  containerOf: {
+    menu: '#product-list',
+    about: '#about',
+    pathfinder: '#finder',
+    finder: '.table-wrapper',
+  },
+};
+  
+export const classNames = {
+  finder: {
+    path: 'path',
+    shortpath: 'short-path',
+  },
+  // CODE ADDED START
+  pages: {
+    active: 'active',
+  }
+};
+  
+  
+export const templates = {
+  finder: Handlebars.compile(document.querySelector(select.templateOf.finder).innerHTML),
+  
+};
